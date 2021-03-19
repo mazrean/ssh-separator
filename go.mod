c69112f1043e96c6dd5eed9301ed63d61eb94467
mazrean <shunsuke.w04@gmail.com>
2021-03-19 23:50:20 +0900

    workspace実装

--- go.mod
+++ go.mod
@@ -3,9 +3,20 @@ module github.com/mazrean/separated-webshell
 go 1.16
 
 require (
+	github.com/Microsoft/go-winio v0.4.16 // indirect
 	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
+	github.com/containerd/containerd v1.4.4 // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v20.10.5+incompatible
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
 	github.com/gliderlabs/ssh v0.3.2
 	github.com/go-playground/validator/v10 v10.4.1
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/labstack/echo/v4 v4.2.1
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	golang.org/x/crypto v0.0.0-20210317152858-513c2a44f670
+	google.golang.org/grpc v1.36.0 // indirect
 )
---
+++ workspace/workspace_impl.go
@@ -0,0 +1,129 @@
+package workspace
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"sync"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+)
+
+var (
+	imageRef string = os.Getenv("IMAGE_URL")
+	opts            = types.ContainerAttachOptions{
+		Stdin:  true,
+		Stdout: true,
+		Stderr: true,
+		Stream: true,
+	}
+	containerMap = sync.Map{}
+)
+
+type containerInfo struct {
+	id         string
+	manageChan chan struct{}
+}
+
+func containerName(userName string) string {
+	return fmt.Sprintf("user-%s", userName)
+}
+
+type Workspace struct {
+	cli *client.Client
+}
+
+func NewWorkspace() (*Workspace, error) {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		return nil, fmt.Errorf("failed to create docker client: %w", err)
+	}
+
+	ctx := context.Background()
+	reader, err := cli.ImagePull(ctx, imageRef, types.ImagePullOptions{})
+	io.Copy(os.Stdout, reader)
+
+	return &Workspace{
+		cli: cli,
+	}, nil
+}
+
+func (w *Workspace) Create(ctx context.Context, userName string) error {
+	res, err := w.cli.ContainerCreate(ctx, &container.Config{
+		Image:        imageRef,
+		OpenStdin:    true,
+		AttachStderr: true,
+		AttachStdin:  true,
+		AttachStdout: true,
+		StdinOnce:    true,
+		Volumes:      make(map[string]struct{}),
+	}, nil, &network.NetworkingConfig{}, nil, containerName(userName))
+	if err != nil {
+		return fmt.Errorf("failed to create container: %w", err)
+	}
+
+	containerMap.Store(userName, &containerInfo{
+		id:         res.ID,
+		manageChan: make(chan struct{}, 20),
+	})
+
+	return nil
+}
+
+func (w *Workspace) Connect(ctx context.Context, userName string, stdin io.Reader, stdout io.Writer, stderr io.Writer) error {
+	iContainerInfo, ok := containerMap.Load(userName)
+	if !ok {
+		return errors.New("load container info error")
+	}
+	ctnInfo, ok := iContainerInfo.(*containerInfo)
+	if !ok {
+		return errors.New("parse container info error")
+	}
+
+	if len(ctnInfo.manageChan) >= 20 {
+		return errors.New("too many shell")
+	}
+
+	err := w.cli.ContainerStart(ctx, ctnInfo.id, types.ContainerStartOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to start container: %w", err)
+	}
+
+	ctnInfo.manageChan <- struct{}{}
+	defer func() {
+		<-ctnInfo.manageChan
+	}()
+
+	stream, err := w.cli.ContainerAttach(ctx, ctnInfo.id, opts)
+	if err != nil {
+		return fmt.Errorf("failed to attach container: %w", err)
+	}
+	defer stream.Close()
+
+	outputErr := make(chan error)
+
+	go func() {
+		_, err := stdcopy.StdCopy(stdout, stderr, stream.Reader)
+		outputErr <- err
+	}()
+
+	go func() {
+		defer stream.CloseWrite()
+		io.Copy(stream.Conn, stdin)
+	}()
+
+	resultC, errC := w.cli.ContainerWait(ctx, ctnInfo.id, container.WaitConditionNotRunning)
+	select {
+	case err = <-errC:
+		return fmt.Errorf("failed to wait container: %w", err)
+	case <-resultC:
+	}
+	err = <-outputErr
+	return nil
+}

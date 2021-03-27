e527a4e937aaa01d0358fb514a3df6844c6df965
mazrean <shunsuke.w04@gmail.com>
2021-03-27 13:17:08 +0900

    badger directory

--- repository/db.go
+++ repository/db.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"fmt"
+	"os"
 
 	badger "github.com/dgraph-io/badger/v3"
 )
@@ -11,8 +12,10 @@ var (
 )
 
 func Setup() (*badger.DB, error) {
+	dir := os.Getenv("BADGER_DIR")
+
 	var err error
-	db, err = badger.Open(badger.DefaultOptions("/tmp/badger"))
+	db, err = badger.Open(badger.DefaultOptions(dir))
 	if err != nil {
 		return nil, fmt.Errorf("failed to open db file: %w", err)
 	}

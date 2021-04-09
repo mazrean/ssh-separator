729a840915e99177726be2a68f48641bd015eba5
mazrean <shunsuke.w04@gmail.com>
2021-04-09 23:57:18 +0900

    Passwordのコンストラクタ

--- domain/values/user.go
+++ domain/values/user.go
@@ -14,6 +14,7 @@ type (
 
 var (
 	userNameExpression = regexp.MustCompile(`^[a-zA-Z0-9](?:[a-zA-Z0-9_-]{0,14}[a-zA-Z0-9])?$`)
+	passwordExpression = regexp.MustCompile(`^[a-zA-Z0-9]{8,32}$`)
 )
 
 func NewUserName(userName string) (UserName, error) {
@@ -22,4 +23,12 @@ func NewUserName(userName string) (UserName, error) {
 	}
 
 	return UserName(userName), nil
+}
+
+func NewPassword(password string) (Password, error) {
+	if !passwordExpression.MatchString(password) {
+		return "", errors.New("invalid password")
+	}
+
+	return Password(password), nil
 }

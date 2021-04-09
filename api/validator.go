0c733d46a6c1fbacd43e117ef86991f1d55479db
mazrean <shunsuke.w04@gmail.com>
2021-04-10 00:30:30 +0900

    apiからvalidator排除

--- api/validator.go
+++ api/validator.go
@@ -1,23 +1,11 @@
 package api
 
 import (
-	"regexp"
-
 	"github.com/go-playground/validator/v10"
-)
-
-var (
-	userNameExpression = regexp.MustCompile(`^[a-zA-Z0-9](?:[a-zA-Z0-9_-]{0,14}[a-zA-Z0-9])?$`)
 )
 
 func NewValidator() *validator.Validate {
 	v := validator.New()
 
-	v.RegisterValidation("userName", userNameValidator)
-
 	return v
-}
-
-func userNameValidator(fl validator.FieldLevel) bool {
-	return userNameExpression.MatchString(fl.FieldName())
 }

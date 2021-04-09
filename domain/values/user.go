461c5bec8c602d797c2ab407960f90e8697edc30
mazrean <shunsuke.w04@gmail.com>
2021-04-09 23:52:09 +0900

    UserNameのコンストラクタ

--- domain/values/user.go
+++ domain/values/user.go
@@ -1,8 +1,25 @@
 package values
 
+import (
+	"errors"
+	"regexp"
+)
+
 type (
 	UserName string
 	Password string
 	// HashedPassword 不明時""
 	HashedPassword string
 )
+
+var (
+	userNameExpression = regexp.MustCompile(`^[a-zA-Z0-9](?:[a-zA-Z0-9_-]{0,14}[a-zA-Z0-9])?$`)
+)
+
+func NewUserName(userName string) (UserName, error) {
+	if !userNameExpression.MatchString(userName) {
+		return "", errors.New("invalid user name")
+	}
+
+	return UserName(userName), nil
+}

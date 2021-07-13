dadd27d83e2107bcec58ecfdb4ef69aa88b18532
mazrean <shunsuke.w04@gmail.com>
2021-07-13 17:32:41 +0900

    暫定

--- repository/badger/transaction_test.go
+++ repository/badger/transaction_test.go
@@ -1,6 +1,9 @@
 package badger
 
-import "testing"
+import (
+	"context"
+	"testing"
+)
 
 func TestTransaction(t *testing.T) {
 	t.Parallel()
@@ -18,6 +21,22 @@ func testTransaction(t *testing.T) {
 		t.Errorf("failed to create test db: %w", err)
 	}
 	defer close()
+
+	transaction := NewTransaction(db)
+
+	tests := []struct{
+		description string
+		ctx context.Context
+		fn func(context.Context) error
+		err error
+	}{}
+
+	for _, test := range tests {
+		t.Run(test.description, func(t *testing.T) {
+			err := transaction.Transaction(test.ctx, test.fn)
+
+		})
+	}
 }
 
 func testRTransaction(t *testing.T) {
